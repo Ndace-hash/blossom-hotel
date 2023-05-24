ccc8755bfd4a490cc431f2e61a19018ac6e3a2d9
Zaccheaus <zaccheausyisa00@gmail.com>
2023-05-24 05:00:35 +0100

    feat: added hero cards

--- src/components/AppHero.jsx
+++ src/components/AppHero.jsx
@@ -1,24 +1,28 @@
 import HeroBanner from "../assets/hero_banner.jpg";
+import HeroCards from "./HeroCards";
 
 const AppHero = () => {
   return (
-    <section>
-      <div className="w-full  relative flex justify-center">
-        <h1 className="font-black uppercase text-white text-7xl translate-y-[25px] z-10">
-          stay with comfort
-        </h1>
-        <div className="mt-16 w-full h-[450px] overflow-hidden absolute opacity-90">
-          <img
-            src={HeroBanner}
-            alt="banner - house with a pool"
-            className="w-full h-full object-cover "
-          />
+    <>
+      <section>
+        <div
+          className="mt-16 w-full h-[450px]  relative opacity-90"
+          style={{
+            backgroundImage: `url(${HeroBanner})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <h1 className="font-black uppercase text-white text-7xl text-center -translate-y-[40px] z-10 drop-shadow-lg">
+            stay with comfort
+          </h1>
           <button className="uppercase absolute bottom-0 left-0 bg-[#ff4400e5] font-semibold text-white text-sm py-3 px-6">
             find rooms for a stay
           </button>
         </div>
-      </div>
-    </section>
+        <HeroCards />
+      </section>
+    </>
   );
 };
 
---
+++ src/components/HeroCards.jsx
@@ -0,0 +1,52 @@
+const HeroCards = () => {
+  return (
+    <section className="flex items-center justify-between z-50 mt-6 mb-12">
+      <article className="h-[200px]  w-[200px] bg-green-300 flex justify-center flex-col px-4">
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Destination.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Luxury.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Respite.
+        </span>
+      </article>
+      <article className="h-[200px]  w-[200px] bg-pink-200 flex justify-center flex-col px-4">
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Destination.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Luxury.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Respite.
+        </span>
+      </article>
+      <article className="h-[200px]  w-[200px] bg-yellow-100 flex justify-center flex-col px-4">
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Destination.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Luxury.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Respite.
+        </span>
+      </article>
+      <article className="h-[200px]  w-[200px] bg-yellow-400 flex justify-center flex-col px-4">
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Destination.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Luxury.
+        </span>
+        <span className="uppercase text-xl font-bold text-[#120601]">
+          Respite.
+        </span>
+      </article>
+    </section>
+  );
+};
+
+export default HeroCards;
