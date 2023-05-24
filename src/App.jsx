090a5aeb97c0d8a8dcce4e67d820d132f5f1510b
Zaccheaus <zaccheausyisa00@gmail.com>
2023-05-24 03:27:02 +0100

    feat:  added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
-  return <>hello</>;
-}
+import AppHeader from "./components/AppHeader";
+const App = () => {
+  return (
+    <main className="w-full max-w-[1045px] md:px-6 mx-auto">
+      <AppHeader />
+    </main>
+  );
+};
 
 export default App;
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,33 @@
+const AppHeader = () => {
+  return (
+    <header className="text-white flex md:justify-between md:items-center md:py-5 border-b-2 border-[rgba(255,255,255,.1)]">
+      <div>
+        <h1 className="uppercase font-bold text-xl">
+          Blossom<span className="text-orange-400">.</span>
+        </h1>
+      </div>
+      <nav className="flex md:items-center md:justify-center gap-6 opacity-80">
+        <li className="list-none uppercase text-sm font-semibold">
+          <a href="#">Explore</a>
+        </li>
+        <li className="list-none uppercase text-sm font-semibold">
+          <a href="#">Rooms</a>
+        </li>
+        <li className="list-none uppercase text-sm font-semibold">
+          <a href="#">About</a>
+        </li>
+        <li className="list-none uppercase text-sm font-semibold">
+          <a href="#">Activities</a>
+        </li>
+      </nav>
+      <button
+        className="border-2 border-[rgba(255,255,255,.7)] py-2 px-6 flex items-center
+       justify-center text-sm uppercase"
+      >
+        Book Now
+      </button>
+    </header>
+  );
+};
+
+export default AppHeader;
